10837329c5fe4885f3562bd4bf0e1a694da50950
Shashwat_Builds <shashwatwork23@gmail.com>
2026-07-21 16:26:05 +0530

    Time: 300 ms (34.13%), Space: 65.6 MB (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.py
+++ 0584-find-customer-referee/0584-find-customer-referee.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
 def find_customer_referee(customer: pd.DataFrame) -> pd.DataFrame:
-    # Filter rows where referee_id is not equal to 2 OR referee_id is NaN (null)
-    condition = (customer['referee_id'] != 2) | (customer['referee_id'].isna())
-    
-    # Return only the 'name' column as a DataFrame
-    return customer.loc[condition, ['name']]
+    df = customer 
+    result = df[(
+        (df["referee_id"] != 2) | (df["referee_id"].isna())
+    )
+    ][["name"]]
+    return result
