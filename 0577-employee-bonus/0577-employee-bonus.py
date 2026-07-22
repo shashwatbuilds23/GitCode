91b70bb8c1ce17fee39aaacb3238b0d230882dcb
Shashwat_Builds <shashwatwork23@gmail.com>
2026-07-22 16:36:48 +0530

    Time: 320 ms (83.11%), Space: 68.2 MB (86.89%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.py
+++ 0577-employee-bonus/0577-employee-bonus.py
@@ -1,11 +1,5 @@
 import pandas as pd
 
 def employee_bonus(employee: pd.DataFrame, bonus: pd.DataFrame) -> pd.DataFrame:
-    # Left join Employee with Bonus on empId
-    df = employee.merge(bonus, on='empId', how='left')
-    
-    # Filter for bonus < 1000 or missing bonus (NaN)
-    condition = (df['bonus'] < 1000) | (df['bonus'].isna())
-    
-    # Select only 'name' and 'bonus' columns
-    return df.loc[condition, ['name', 'bonus']] 
+    df = pd.merge(employee, bonus, on = 'empId', how = 'left')
+    return df[(df['bonus'] < 1000) | (df['bonus'].isna())][['name', 'bonus']]
